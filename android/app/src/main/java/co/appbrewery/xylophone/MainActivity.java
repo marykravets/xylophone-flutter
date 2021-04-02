a88668045ed2c223ecd7335d0004675808a0ed32
Mariana <mary.kravets@gmail.com>
2021-04-02 12:58:18 -0400

    migration

--- android/app/src/main/java/co/appbrewery/xylophone/MainActivity.java
+++ android/app/src/main/java/co/appbrewery/xylophone/MainActivity.java
@@ -1,9 +1,24 @@
 package co.appbrewery.xylophone;
 
-import android.os.Bundle;
 import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugins.GeneratedPluginRegistrant;
+import androidx.annotation.NonNull;
+import io.flutter.plugin.common.MethodChannel;
 
 public class MainActivity extends FlutterActivity {
 
+    private static final String CHANNEL = "samples.flutter.dev/battery";
+
+    @Override
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+        new MethodChannel(
+                flutterEngine.getDartExecutor().getBinaryMessenger(),
+                CHANNEL)
+                .setMethodCallHandler(
+                        (call, result) -> {
+                        }
+                );
+    }
 }
